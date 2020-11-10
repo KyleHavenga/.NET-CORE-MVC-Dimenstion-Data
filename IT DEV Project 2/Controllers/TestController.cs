515d65c5474e27853ef32b0f1b2ebb7a401f4769
KyleHavenga <kyle.havenga888@gmail.com>
2020-11-10 23:56:00 +0200

    First initial commit base created

---
+++ IT DEV Project 2/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IT_DEV_Project_2.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string StringOut(int id)
+        {
+            return $"StringOut Action {id}";
+        }
+    }
+}
